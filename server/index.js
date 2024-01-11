bf4f9ff9a3e24e06dbb390d211fd0813ff452d88
Gautam Dhakate <106535248+Gautam-121@users.noreply.github.com>
2024-01-11 11:22:11 +0530

    primary File indexJs with remove all unneccesary file

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const verifyRequest = require('./middleware/verifyRequest.js');
 const proxyRouter = require('./routes/app_proxy/index.js');
 const router = require('./routes/index.js');
 const webhookRegistrar = require('./webhooks/index.js');
-const multer = require("multer")
+const fileUpload = require("express-fileupload");
 require('events').EventEmitter.prototype._maxListeners = 70;
 
 
@@ -36,139 +36,142 @@ webhookRegistrar();
 
 const app = express();
 app.use(cors())
-// app.use(multer().any())
+app.use(fileUpload({
+  useTempFiles: true
+}))
+
 
 const start = async () => {
-  // Initialize Payload
-  await payload.init({
-    secret: process.env.PAYLOAD_SECRET,
-    express: app,
-    onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
-    },
-  })
-}
-const createServer = async (root = process.cwd()) => {
-
-  app.disable("x-powered-by");
-  applyAuthMiddleware(app);
-
-  console.log("Enter Inside The createServer")
-
-  // Incoming webhook requests
-  app.post(
-    "/webhooks/:topic",
-    express.text({ type: "*/*" }),
-    async (req, res) => {
-      const { topic } = req.params || "";
-      const shop = req.headers["x-shopify-shop-domain"] || "";
-
+  try {
+    await payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      express: app,
+      onInit: async () => {
+        payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+      },
+    });
+  
+    const root = process.cwd()
+    app.disable("x-powered-by");
+  
+    applyAuthMiddleware(app);
+  
+    console.log("Enter Inside The createServer App")
+  
+    // Incoming webhook requests
+    app.post(
+      "/webhooks/:topic",
+      express.text({ type: "*/*" }),
+      async (req, res) => {
+        const { topic } = req.params || "";
+        const shop = req.headers["x-shopify-shop-domain"] || "";
+  
+        try {
+          await shopify.webhooks.process({
+            rawBody: req.body,
+            rawRequest: req,
+            rawResponse: res,
+          });
+          console.log(`--> Processed ${topic} webhook for ${shop}`);
+        } catch (e) {
+          console.error(
+            `---> Error while registering ${topic} webhook for ${shop}`,
+            e
+          );
+          if (!res.headersSent) {
+            res.status(500).send(error.message);
+          }
+        }
+      }
+    );
+  
+    app.use(express.json());
+  
+    app.post("/graphql", verifyRequest, async (req, res) => {
       try {
-        await shopify.webhooks.process({
-          rawBody: req.body,
+        const sessionId = await shopify.session.getCurrentId({
+          isOnline: true,
           rawRequest: req,
           rawResponse: res,
         });
-        console.log(`--> Processed ${topic} webhook for ${shop}`);
+        const session = await sessionHandler.loadSession(sessionId);
+        const response = await shopify.clients.graphqlProxy({
+          session,
+          rawBody: req.body,
+        });
+        res.status(200).send(response.body);
       } catch (e) {
-        console.error(
-          `---> Error while registering ${topic} webhook for ${shop}`,
-          e
-        );
-        if (!res.headersSent) {
-          res.status(500).send(error.message);
-        }
+        console.error(`---> An error occured at GraphQL Proxy`, e);
+        res.status(403).send(e)
       }
-    }
-  );
-
-  app.use(express.json());
-
-  app.post("/graphql", verifyRequest, async (req, res) => {
-    try {
-      const sessionId = await shopify.session.getCurrentId({
-        isOnline: true,
-        rawRequest: req,
-        rawResponse: res,
-      });
-      const session = await sessionHandler.loadSession(sessionId);
-      const response = await shopify.clients.graphqlProxy({
-        session,
-        rawBody: req.body,
-      });
-      res.status(200).send(response.body);
-    } catch (e) {
-      console.error(`---> An error occured at GraphQL Proxy`, e);
-      res.status(403).send(e);
-    }
-  });
-
-  app.use(csp);
-  app.use(isShopActive);
-  // If you're making changes to any of the routes, please make sure to add them in `./client/vite.config.cjs` or it'll not work.
-  app.use("/apps",  router); //Verify user route requests
-  app.use("/proxy_route", verifyProxy, proxyRouter); //MARK:- App Proxy routes
-
-  app.post("/gdpr/:topic", verifyHmac, async (req, res) => {
-    const { body } = req;
-    const { topic } = req.params;
-    const shop = req.body.shop_domain;
-
-    console.warn(`--> GDPR request for ${shop} / ${topic} recieved.`);
-
-    let response;
-    switch (topic) {
-      case "customers_data_request":
-        response = await customerDataRequest(topic, shop, body);
-        break;
-      case "customers_redact":
-        response = await customerRedact(topic, shop, body);
-        break;
-      case "shop_redact":
-        response = await shopRedact(topic, shop, body);
-        break;
-      default:
-        console.error(
-          "--> Congratulations on breaking the GDPR route! Here's the topic that broke it: ",
-          topic
-        );
-        response = "broken";
-        break;
-    }
-
-    if (response.success) {
-      res.status(200).send();
-    } else {
-      res.status(403).send("An error occured");
-    }
-  });
-
-  if (!isDev) {
-    const compression = await import("compression").then(
-      ({ default: fn }) => fn
-    );
-    const serveStatic = await import("serve-static").then(
-      ({ default: fn }) => fn
-    );
-    const fs = await import("fs");
-
-    app.use(compression());
-    app.use(serveStatic(resolve("dist/client")));
-    app.use("/*", (req, res, next) => {
-      res
-        .status(200)
-        .set("Content-Type", "text/html")
-        .send(fs.readFileSync(`${root}/dist/client/index.html`));
     });
+  
+    app.use(csp);
+    app.use(isShopActive)
+    // If you're making changes to any of the routes, please make sure to add them in `./client/vite.config.cjs` or it'll not work.
+    app.use("/apps" ,  router); //Verify user route requests
+    app.use("/proxy_route", verifyProxy, proxyRouter); //MARK:- App Proxy routes
+  
+    app.post("/gdpr/:topic", verifyHmac, async (req, res) => {
+      const { body } = req;
+      const { topic } = req.params;
+      const shop = req.body.shop_domain;
+  
+      console.warn(`--> GDPR request for ${shop} / ${topic} recieved.`);
+  
+      let response;
+      switch (topic) {
+        case "customers_data_request":
+          response = await customerDataRequest(topic, shop, body);
+          break;
+        case "customers_redact":
+          response = await customerRedact(topic, shop, body);
+          break;
+        case "shop_redact":
+          response = await shopRedact(topic, shop, body);
+          break;
+        default:
+          console.error(
+            "--> Congratulations on breaking the GDPR route! Here's the topic that broke it: ",
+            topic
+          );
+          response = "broken";
+          break;
+      }
+  
+      if (response.success) {
+        res.status(200).send();
+      } else {
+        res.status(403).send("An error occured");
+      }
+    });
+  
+    if (!isDev) {
+      const compression = await import("compression").then(
+        ({ default: fn }) => fn
+      );
+      const serveStatic = await import("serve-static").then(
+        ({ default: fn }) => fn
+      );
+      const fs = await import("fs");
+  
+      app.use(compression());
+      app.use(serveStatic(resolve("dist/client")));
+      app.use("/*", (req, res, next) => {
+        res
+          .status(200)
+          .set("Content-Type", "text/html")
+          .send(fs.readFileSync(`${root}/dist/client/index.html`));
+      });
+    }
+  
+    app.listen(PORT, () => {
+      console.log(`--> Running on ${PORT}`);
+    });
+  } catch (error) {
+    console.log("error is " , error)
   }
-
-  return { app };
 };
 
-createServer().then(({ app }) => {
-  app.listen(PORT, () => {
-    console.log(`--> Running on ${PORT}`);
-  });
-});
 
 start()
