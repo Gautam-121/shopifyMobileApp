f4c5f5822ad400ce8cccb0099976c8ed556e18c5
Gautam-121 <gautamdhakate1234@gmail.com>
2024-02-07 10:50:44 +0530

    updated Server Code

--- server/index.js
+++ server/index.js
@@ -167,10 +167,10 @@ const start = async () => {
       });
     }
 
-    const localIp = "192.168.1.135"
+    const localIp = "192.168.1.74"
   
     app.listen(PORT,localIp ,() => {
-      console.log(`Server running at http://localhost:${process.env.PORT}`);
+      console.log(`Server running at http://${localIp}:${process.env.PORT}`);
     });
     
   } catch (error) {
